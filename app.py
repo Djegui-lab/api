50f87a97a814ee060ef9f0fe6389fd29138850f6
Djegui-lab <dwague44@gmail.com>
2023-09-30 14:55:41 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st 
+import pandas as pd 
+
+
+def load_data():
+    # Définissez les autorisations et l'accès au fichier JSON de clé d'API
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("test-wague-9a205da3c6ca.json", scope)
+
+    # Authentification avec les informations d'identification
+    gc = gspread.authorize(credentials)
+
+    # Ouvrir la feuille de calcul par son nom ou URL
+    # Remplacez "Nom de votre feuille" par le nom de votre propre feuille ou l'URL
+    worksheet = gc.open("courtier").sheet1
+
+    # Lire les données de la feuille de calcul
+    data = worksheet.get_all_values()
+
+    # Convertir les données en un DataFrame pandas
+    df = pd.DataFrame(data[1:], columns=data[0])
+
+    return df
+
+data = load_data()
+
+
+st.title("Application de données Google Sheets avec Streamlit")
+
+# Afficher les données dans une table
+st.write("Données depuis Google Sheets :")
+st.write(data)
+
+
+
+
+
+
+
+
